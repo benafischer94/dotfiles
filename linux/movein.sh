fa5e88a2887cb54af06abd7d3008e55943ce7027
Benjamin Fischer <bfischer@outlook.com>
2022-03-31 15:24:20 +0000

    Update movin.sh to be more portable * Fix issue with profile linking * Most commands needed to be ran as sudo * move dhcp.sh to a scripts folder   * Will be linking all scripts this way going forward

--- linux/movein.sh
+++ linux/movein.sh
@@ -1,34 +1,65 @@
 #! /usr/bin/bash
 
+# Profile
+echo "Spitting out our profile"
+cat "$PWD/.profile" > $HOME/.profile
+
+echo "Linking our scripts"
+ln -s -f "$PWD/scripts/" $HOME/scripts
+
+echo "Git config"
+cat "$PWD/.gitconfig" > $HOME/.gitconfig
+
 # Oh my posh!
-wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
-chmod +x /usr/local/bin/oh-my-posh
+echo "Pulling Posh!"
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh >/dev/null
+sudo chmod +x /usr/local/bin/oh-my-posh >/dev/null
+# Need to get nerdfont setup.
 
 # Move around the shared resources
-ln -s ../shared/bfischer.omp.json ~/.poshthemes/bfischer.omp.json
+echo "Moving around shared resources"
+if ! [ -d "$HOME/.poshthemes" ] ; then
+  mkdir $HOME/.poshthemes > /dev/null
+fi
+  ln -s -f "$PWD/../shared/bfischer.omp.json" $HOME/.poshthemes/bfischer.omp.json
 
 # Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-# Need to automate accepting default.s
+#echo "Downloading rust!"
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# Need to automate accepting defaults, and some version checking
 
 # Personal computer usage python (need to make so this doesn't happen on servers.)
-ln -sf /usr/bin/python3 /usr/bin/python
-apt install ansible nmap python3-venv python3-pip yarn npm
+echo "Makeing sure python3 is the default python"
+sudo ln -sf /usr/bin/python3 /usr/bin/python
+
+# Needful downloads
+echo "Fetching Ansible, NMAP, VENV, PIP, Yarn, and NPM"
+sudo apt-get install ansible nmap python3-venv python3-pip yarn npm -y >/dev/null
 
 # Terraform
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-ansible-galaxy install geerlingguy.postgresql robertdebock.rpmfusion
-ansible-galaxy collection install community.general
+echo "Terraforming"
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - >/dev/null
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main" >/dev/null
+
+# Ansible
+echo "Some basic ansible needfuls"
+ansible-galaxy install geerlingguy.postgresql robertdebock.rpmfusion > /dev/null
+ansible-galaxy collection install community.general >/dev/null
 
 # Azure
 #Universe package is pretty outdated, use MS official instead
-sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+echo "Setting up Azure"
+sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg >/dev/null
 curl -sL https://packages.microsoft.com/keys/microsoft.asc |
     gpg --dearmor |
     sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
-    sudo tee /etc/apt/sources.list.d/azure-cli.list
-apt-get update
-apt-get install azure-cli
+    sudo tee /etc/apt/sources.list.d/azure-cli.list >/dev/null
+sudo apt-get update >/dev/null
+sudo apt-get install azure-cli >/dev/null
+
+# Activate the new profile
+echo "Don't forget to source $HOME/.profile"
+
+echo "Done!"
