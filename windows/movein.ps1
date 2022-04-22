e38b9643e65dcc8eb3101f236b781530e3089653
Ben Fischer <benfischer@outlook.com>
2022-04-21 23:07:49 -0400

    add sharepoint to PSH

--- windows/movein.ps1
+++ windows/movein.ps1
@@ -7,4 +7,7 @@ Find-module -Name VMware.PowerCLI -y
 Install-Module -Name VMware.PowerCLI -Scope CurrentUser -y
 #(Y) to install from PSGallery
 Set-PowerCLIConfiguration -Scope AllUsers -ParticipateInCeip $true -InvalidCertificateAction Ignore -y
+# Sharepoint Online management
 #(Y) To run the update
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell -Scope CurrentUser
+#(Y) To install
