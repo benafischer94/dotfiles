b06e5b1c63a0f897cbde5d916f2cdf71dcef0e68
Ben Fischer <benfischer@outlook.com>
2024-09-30 11:23:54 -0400

    Add basic ssh config and rsat to windows tools
    Add a basic ssh config to the shared files
    Adds support for RSAT tools to the moving script for windows.

--- windows/movein.ps1
+++ windows/movein.ps1
@@ -13,3 +13,23 @@ Set-PowerCLIConfiguration -Scope AllUsers -ParticipateInCeip $true -InvalidCerti
 #(Y) To run the update
 Install-Module -Name Microsoft.Online.SharePoint.PowerShell -Scope CurrentUser
 #(Y) To install
+
+#ToDo a bool for checking whether to install RSAT Tools.
+$rsat = Read-Host -Prompt "Select from 0,1,or 2 for the following:
+0: No RSAT tools
+1: Just DHCP and DNS manager
+2: All RSAT tools"
+
+if ($rsat -eq '0') {
+} elseif ($rsat -eq '1') {
+    Add-WindowsCapability -Online -Name Rsat.DHCP.Tools
+    Add-WindowsCapability -Online -Name Rsat.Dns.Tools
+} elseif ($rsat -eq '2') {
+    Get-WindowsCapability -Name RSAT* -Online | Add-WindowsCapability -Online
+} else {
+    Write-Output "Invalid choice."
+}
+
+# Create and copy ssh .config if not existing
+[System.IO.Directory]::CreateDirectory("~\.ssh")
+mv ..\shared\.ssh\config ~\.ssh\config
